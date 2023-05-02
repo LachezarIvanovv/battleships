2e78ae5956f7af8219c8eecd072f233aa669c902
LachezarIvanovv <lachezar.ivanov.0888@abv.bg>
2023-05-02 13:18:13 +0300

    Initial commit

---
+++ src/main/java/com/example/battleships/models/User.java
@@ -0,0 +1,67 @@
+package com.example.battleships.models;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "users")
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long ind;
+
+    @Column(name = "full_name", nullable = false)
+    private String fullName;
+
+    @Column(unique = true, nullable = false)
+    private String username;
+
+    @Column(nullable = false)
+    private String password;
+
+    @Column(unique = true, nullable = false)
+    private String email;
+
+    public User() {
+    }
+
+    public long getInd() {
+        return ind;
+    }
+
+    public String getFullName() {
+        return fullName;
+    }
+
+    public User setFullName(String fullName) {
+        this.fullName = fullName;
+        return this;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public User setUsername(String username) {
+        this.username = username;
+        return this;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public User setPassword(String password) {
+        this.password = password;
+        return this;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public User setEmail(String email) {
+        this.email = email;
+        return this;
+    }
+}
